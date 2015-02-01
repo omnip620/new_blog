6a6bc1ec93c35c9a5e82d4983081130af5c2ab95
pzc <412726700@qq.com>
2015-02-01 22:30:51 +0800

    remove markdown syntax

--- controllers/index.js
+++ controllers/index.js
@@ -6,33 +6,41 @@
 var Article = require('../models/article');
 var _ = require('lodash');
 var moment = require('moment');
+var md = require('markdown-it')({html: true, linkify: true, typographer: true});
+
+function page(num, callback) {
+  Article.find({}, '_id title top source tags views comments updated_at created_at content', {
+    'sort': '-updated_at',
+    skip: (num - 1) * 10,
+    limit: 10
+  }, function (err, articles) {
+    if (err) {
+      return callback(err)
+    }
+    articles.forEach(function (o) {
+      if (o.content) {
+        o.content = md.render(o.content.substring(0, 170)).replace(/<[^>]+>/gi, '') + '...';
+      }
+    });
+    callback(null, articles)
+  });
+}
 
 exports.show = function (req, res) {
-  Article.find({}, '_id title top source tags views comments updated_at created_at content',{'sort':'-updated_at',limit:10},function (err, articles) {
+  page(1, function (err, articles) {
     if (err) {
       return res.redirect('/404')
     }
-    articles.forEach(function(o){
-      if (o.content) {
-        o.content = o.content.substring(0, 170) + '...';
-      }
-    });
     return res.render('index', {articles: articles});
-  });
+  })
 };
 
-exports.page=function(req,res){
-  var pageNum=parseInt(req.query.num,10);
-  console.log(pageNum);
-  Article.find({}, '_id title top source tags views comments updated_at created_at content',{'sort':'-updated_at',skip:(pageNum-1)*10,limit:10},function (err, articles) {
+exports.page = function (req, res) {
+  var pageNum = parseInt(req.query.num, 10);
+  page(pageNum, function (err, articles) {
     if (err) {
-      return res.json('404',err)
+      return res.json('404', err)
     }
-    articles.forEach(function(o){
-      if (o.content) {
-        o.content = o.content.substring(0, 170) + '...';
-      }
-    });
     return res.json(articles);
-  });
+  })
 };
