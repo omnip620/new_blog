6c41ed59e59b5ee5dce2987f47dfbd024f6ed5ef
p412726700 <412726700@qq.com>
2015-02-24 21:09:36 +0800

    update synccomments.js

--- common/synccomments.js
+++ common/synccomments.js
@@ -7,5 +7,5 @@ exports.index = function (req, res) {
   console.log(req.body);
   var secret = '532e232e6c639993343c09668e45b621',
     short_name = 'panblog';
-     return res.json(req.body);
+    return res.json(200);
 };
