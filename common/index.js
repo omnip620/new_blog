e2839cc263294ed29e58c9f91dadfe3607f94f0a
p412726700 <412726700@qq.com>
2015-02-24 20:20:05 +0800

    update index.js

--- common/index.js
+++ common/index.js
@@ -2,7 +2,9 @@
  * Created by panew on 15-2-24.
  */
 var express = require('express');
+var sc=require('./synccomments.js');
+
 
 var router = express.Router();
 
-router.get('/sc',require('./synccomments.js').index);
+router.get('/sc',sc.index);
