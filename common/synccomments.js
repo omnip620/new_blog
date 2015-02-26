9d06b1eb85440c70d56bcd465cba0f3673f9c261
pzc <412726700@qq.com>
2015-02-26 13:50:48 +0800

    http get

--- common/synccomments.js
+++ common/synccomments.js
@@ -2,9 +2,20 @@
  * Created by panew on 15-2-24.
  */
 
+var http = require('http');
 exports.index = function (req, res) {
   console.log(req.action)
   var secret = '532e232e6c639993343c09668e45b621',
-    short_name = 'panblog';
-    return res.json(200);
+    short_name = 'panblog',
+    url = 'http://api.duoshuo.com/log/list.json?short_name=' + short_name + '&secret=' + secret;
+  http.get(url, function (res) {
+    console.log("Got response: " + res.statusCode);
+    res.on('data', function (data) {
+      console.log("Got data: " + data);
+    });
+  }).on('error', function (e) {
+    console.log("Got error: " + e.message);
+  });
+
+  return res.json(200);
 };
