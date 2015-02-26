43560eba86e2d4764c974e18833a882adde3cfe2
pzc <412726700@qq.com>
2015-02-26 11:16:55 +0800

    debug

--- common/synccomments.js
+++ common/synccomments.js
@@ -2,9 +2,8 @@
  * Created by panew on 15-2-24.
  */
 
-
 exports.index = function (req, res) {
-  console.log(req);
+  console.log(req.body)
   var secret = '532e232e6c639993343c09668e45b621',
     short_name = 'panblog';
     return res.json(200);
