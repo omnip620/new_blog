71d38333ae4752a6824ef5dc2a6ff5e07940f47a
pzc <412726700@qq.com>
2015-02-26 18:19:05 +0800

    fix bug

--- common/synccomments.js
+++ common/synccomments.js
@@ -4,35 +4,38 @@
 
 var http = require('http');
 var Article = require('./../models/article');
+var _ = require('lodash');
 exports.index = function (req, res) {
   console.log(req.body);
   var secret = '532e232e6c639993343c09668e45b621',
     short_name = 'panblog',
-    url = 'http://api.duoshuo.com/log/list.json?short_name=' + short_name + '&secret=' + secret+'&limit=3&order=desc',
+    url = 'http://api.duoshuo.com/log/list.json?short_name=' + short_name + '&secret=' + secret + '&limit=3&order=desc',
     data = '', item = '';
 
   http.get(url, function (res) {
     console.log("Got response: " + res.statusCode);
     res.on('data', function (results) {
-      data =JSON.parse(results);
+      data = JSON.parse(results);
       console.log("Got data: " + data);
       item = data.response;
-      item=item[0];
+      item = item[0];
       if (item.action === "delete") {
-        Article.find({comment_ids: {$in: item.meta}}, function (err, result) {
+        Article.find({comment_ids: {$in: item.meta}}, function (err, articles) {
           if (err) {
             console.log('err', err);
           }
           else {
-            console.log(result);
+            articles.forEach(function (article) {
+              article._doc.comment_ids = _.difference(article._doc.comment_ids, item.meta)
+            })
+
+            console.log(articles);
           }
         })
       }
       else if (item.action === 'create' && item.meta.status === 'approved') {
         Article.findById(item.meta.thread_key).exec()
           .then(function (article) {
-            console.log(article)
-            console.log(typeof article.comment_ids)
             article.comment_ids.push(item.meta.post_id);
             article.save();
           })
@@ -40,7 +43,6 @@ exports.index = function (req, res) {
             console.log(err);
           })
       }
-
     });
   }).on('error', function (e) {
     console.log("Got error: " + e.message);
