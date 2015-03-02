3d6b3ebab944e2851bd1cd17caf8319f29d16bd6
pzc <412726700@qq.com>
2015-03-02 13:16:01 +0800

    remove console log

--- app.js
+++ app.js
@@ -86,7 +86,6 @@ app.use(session({
 
 //right sidebar data bind
 app.use(function (req, res, next) {
-  console.log(app.get('env'));
   Article.find({}, 'title views', {sort: '-views', limit: 5}).exec()
     .then(function (result) {
       res.locals.topViews = result;
@@ -94,7 +93,7 @@ app.use(function (req, res, next) {
     })
     .then(function (result) {
       res.locals.tagList = result;
-      return Article.find({}, 'title comment_ids', {limit: 5,sort:'-comment_ids'}).exec();
+      return Article.find({}, 'title comment_ids', {limit: 5, sort: '-comment_ids'}).exec();
     })
     .then(function (result) {
       res.locals.topComments = result;
