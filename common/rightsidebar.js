06fd3bbb0ca4661c55867aaf4e5ba4693db740c0
p412726700 <412726700@qq.com>
2015-04-22 15:32:57 +0800

    update rightsidebar.js

--- common/rightsidebar.js
+++ common/rightsidebar.js
@@ -5,20 +5,18 @@ var Article = require('./../models/article');
 var Tag = require('./../models/tag');
 
 module.exports = function (req, res, next) {
-  Article.find({}, 'title views', {sort: '-views', limit: 5}).exec()
-    .then(function (result) {
-      res.locals.topViews = result;
-      return Tag.find({}).exec();
-    })
-    .then(function (result) {
-      res.locals.tagList = result;
-      return Article.find({}, 'title comment_ids', {limit: 5, sort: '-comment_ids'}).exec();
-    })
-    .then(function (result) {
-      res.locals.topComments = result;
+   Promise.
+    join(
+    Article.find({}, 'title views', {sort: '-views', limit: 5}).exec(),
+    Tag.find({}).exec(),
+    Article.find({}, 'title comment_ids', {limit: 5, sort: '-comment_ids'}).exec(),
+    function (topviews, taglist, topcomments) {
+      res.locals.topViews = topviews;
+      res.locals.tagList = taglist;
+      res.locals.topComments = topcomments;
       next();
     })
-    .then(null, function (err) {
+    .catch(function (err) {
       return res.redirect('/404')
     })
 };
